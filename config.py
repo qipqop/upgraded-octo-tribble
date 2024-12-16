3fe86d7fcf6d2c2ea34fe672844d18bbed268657
Metatron <qipqop@gmail.com>
2024-12-16 15:15:45 +0300

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ pyCryptoToken = "305848:AACZ6n5EZXqkPMDf45LH1IbW2Wpuou9Br6j"
 
 menu_structure = {
     'Южно-Сахалинск': [
-        {'text': 'Вы уверены? Юж.Сахалинск', 'callback': 'subca_4'}
+        {'text': 'ПОДТВЕРДИТЕ: Да! Юж.Сахалинск', 'callback': 'subca_4'}
     ],
     'Петропавловск-Камчатский': [
         {'text': 'Подкатегория 3.1', 'callback': 'subca_7'},
